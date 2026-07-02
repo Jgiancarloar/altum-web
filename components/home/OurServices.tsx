36242ddd9ff1558786f64e51a5fa3b434d78b47e
Giancarlo <jgiancarloalvarezr@gmail.com>
2026-07-02 16:06:35 -0500

    actualizacion

---
+++ components/home/OurServices.tsx
@@ -0,0 +1,9 @@
+export default function OurServices() {
+    return (
+        <section className="px-5 py-10">
+            <div>
+                <h2>Our Services</h2>
+            </div>
+        </section>
+    );
+}
